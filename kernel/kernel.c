8f0ef3b8f9f1e8b2b7f7efb81f12197dff452110
omer54463 <omer@lubin.ws>
2023-05-12 16:05:08 +0300

    RENAME limine_frame_buffer limine_framebuffer

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,7 +4,7 @@
 
 void entry(void);
 
-static volatile struct limine_framebuffer_request framebuffer_request = {
+static volatile struct limine_framebuffer_request limine_framebuffer_request = {
     .id = LIMINE_FRAMEBUFFER_REQUEST, .revision = 0};
 
 static volatile struct limine_entry_point_request entry_request = {
@@ -21,16 +21,16 @@ static void hcf(void) {
 }
 
 void entry(void) {
-    if (framebuffer_request.response == NULL ||
-        framebuffer_request.response->framebuffer_count < 1) {
+    if (limine_framebuffer_request.response == NULL ||
+        limine_framebuffer_request.response->framebuffer_count < 1) {
         goto cleanup;
     }
 
-    struct limine_framebuffer* limine_frame_buffer =
-        framebuffer_request.response->framebuffers[0];
+    struct limine_framebuffer* limine_framebuffer =
+        limine_framebuffer_request.response->framebuffers[0];
 
     frame_buffer_t frame_buffer;
-    frame_buffer_initialize(&frame_buffer, limine_frame_buffer);
+    frame_buffer_initialize(&frame_buffer, limine_framebuffer);
 
     terminal_t terminal;
     terminal_initialize(&terminal, &frame_buffer);
